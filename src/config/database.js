5beed853fa69c051c31a19a176e12df602565812
Helder Nicollas <nicollashelder@gmail.com>
2023-04-02 16:36:18 -0300

    deploy new feature (note title alteration)

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import mongoose from "mongoose";
-// HOST: `mongodb+srv://helder:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_USER}.kgnjbaq.mongodb.net/javascriptNotes?retryWrites=true&w=majority`
 
-mongoose.connect(`mongodb://127.0.0.1:27017/javascriptNotes`, {
+mongoose.connect(`mongodb+srv://helder:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_USER}.kgnjbaq.mongodb.net/javascriptNotes?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     autoIndex: true
